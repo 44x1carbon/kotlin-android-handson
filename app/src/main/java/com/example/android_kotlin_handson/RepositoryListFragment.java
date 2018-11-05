140be211189552951cec027e19a27881271b3e09
44x1carbon <b4079@oic.jp>
2018-11-06 01:13:04 +0900

    取得したリポジトリをとりあえず一覧表示

--- app/src/main/java/com/example/android_kotlin_handson/RepositoryListFragment.java
+++ app/src/main/java/com/example/android_kotlin_handson/RepositoryListFragment.java
@@ -1,13 +1,17 @@
 package com.example.android_kotlin_handson;
 
-import android.content.Context;
-import android.net.Uri;
 import android.os.Bundle;
+import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
+import com.example.android_kotlin_handson.api.HashMapSearchRepositoryApi;
+import com.example.android_kotlin_handson.api.SearchRepositoryApi;
+import io.reactivex.android.schedulers.AndroidSchedulers;
 
 /**
  * A simple {@link Fragment} subclass.
@@ -17,15 +21,14 @@ import android.view.ViewGroup;
  * create an instance of this fragment.
  */
 public class RepositoryListFragment extends Fragment {
-    // TODO: Rename parameter arguments, choose names that match
-    // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
     private static final String ARG_LANGUAGE = "ARG_LANGUAGE";
 
 
     private String mLanguage;
+    private SearchRepositoryApi api;
 
     public RepositoryListFragment() {
-        // Required empty public constructor
+        this.api = new HashMapSearchRepositoryApi();
     }
 
     /**
@@ -56,7 +59,23 @@ public class RepositoryListFragment extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_repository_list, container, false);
+        View view = inflater.inflate(R.layout.fragment_repository_list, container, false);
+
+        RecyclerView recyclerView = (RecyclerView) view.findViewById(R.id.recyclerView);
+        recyclerView.setLayoutManager(new LinearLayoutManager(getActivity()));
+
+        return view;
+    }
+
+    @Override
+    public void onActivityCreated(@Nullable Bundle savedInstanceState) {
+        super.onActivityCreated(savedInstanceState);
+
+        api.searchRepositoryByLanguage(mLanguage)
+                .observeOn(AndroidSchedulers.mainThread())
+                .subscribe(repositoryList -> {
+                    RecyclerView recyclerView = getView().findViewById(R.id.recyclerView);
+                    recyclerView.setAdapter(new RecyclerAdapter(getContext(), repositoryList));
+                });
     }
 }
