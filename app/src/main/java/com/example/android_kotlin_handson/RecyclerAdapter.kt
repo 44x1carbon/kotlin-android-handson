5e880afb129df4ded6bb4ec03c9148885ca2d5c9
44x1carbon <b4079@oic.jp>
2018-11-21 23:08:24 +0900

    RecyclerAdapterをKotlinらしく修正

--- app/src/main/java/com/example/android_kotlin_handson/RecyclerAdapter.kt
+++ app/src/main/java/com/example/android_kotlin_handson/RecyclerAdapter.kt
@@ -9,35 +9,29 @@ import android.widget.TextView
 
 import com.example.android_kotlin_handson.model.Repository
 
-class RecyclerAdapter internal constructor(private val mContext: Context, private val mRepositoryList: List<Repository>?) : RecyclerView.Adapter<RecyclerAdapter.ViewHolder>() {
+class RecyclerAdapter internal constructor(
+        mContext: Context,
+        private val mRepositoryList: List<Repository>
+) : RecyclerView.Adapter<RecyclerAdapter.ViewHolder>() {
 
-    private val mInflater: LayoutInflater
-
-    init {
-        mInflater = LayoutInflater.from(mContext)
-    }
+    private val mInflater: LayoutInflater = LayoutInflater.from(mContext)
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
         return ViewHolder(mInflater.inflate(R.layout.list_item, parent, false))
     }
 
     override fun onBindViewHolder(holder: ViewHolder, position: Int) {
-        if (mRepositoryList != null && mRepositoryList.size > position && mRepositoryList[position] != null) {
+        if (mRepositoryList.size > position) {
             val (fullName) = mRepositoryList[position]
             holder.textView.text = fullName
         }
     }
 
     override fun getItemCount(): Int {
-        return mRepositoryList?.size ?: 0
+        return mRepositoryList.size
     }
 
-    internal inner class ViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
-
-        var textView: TextView
-
-        init {
-            textView = itemView.findViewById<View>(R.id.repositoryName) as TextView
-        }
+    inner class ViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
+        val textView: TextView = itemView.findViewById<View>(R.id.repositoryName) as TextView
     }
 }
