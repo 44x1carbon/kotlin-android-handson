758721e0da41f4d27b02ba0bec5520a660b2950a
44x1carbon <b4079@oic.jp>
2018-11-21 22:40:29 +0900

    RepositoryをKotlinらしく修正

--- app/src/main/java/com/example/android_kotlin_handson/model/Repository.kt
+++ app/src/main/java/com/example/android_kotlin_handson/model/Repository.kt
@@ -2,7 +2,8 @@ package com.example.android_kotlin_handson.model
 
 import com.google.gson.annotations.SerializedName
 
-class Repository(@field:SerializedName("full_name")
-                 var fullName: String, @field:SerializedName("html_url")
-                 var htmlUrl: String, @field:SerializedName("stargazers_count")
-                 var stargazersCount: Int?)
+data class Repository(
+        @field:SerializedName("full_name") val fullName: String,
+        @field:SerializedName("html_url") val htmlUrl: String,
+        @field:SerializedName("stargazers_count") val stargazersCount: Int
+)
